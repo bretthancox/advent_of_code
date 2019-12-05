d65fbec055fe5333c67c6ac18419fac36c4367ef
bretthancox <brett.hancox@gmail.com>
2019-12-05 18:00:44 -0500

    Rewrote the day2.2 and some of the day3.1 functions to make them more readable and use cond instead of nested if statements.

--- test/advent/core_test.clj
+++ test/advent/core_test.clj
@@ -2,10 +2,6 @@
   (:require [clojure.test :refer :all]
             [advent.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
-
 (deftest day2_1_computer_test
   (testing "Day 2.1"
     (is (= (day2_1 [1,0,0,0,99]) [2,0,0,0,99]))
@@ -22,8 +18,8 @@
 
 (deftest day2_2_test
   (testing "Day 2.1 noun and verb finder"
-    (is (= (day2_2_nounverb [1 5 6 0 99 19690719 1]) [0 5]))
-    (is (= (day2_2_result (day2_2_nounverb [1 5 6 0 99 19690719 1])) 5))
+    (is (= (day2_2_nounverb [1 5 6 0 99 19690719 1] 99 19690720) [0 5]))
+    (is (= (day2_2_result (day2_2_nounverb [1 5 6 0 99 19690719 1] 99 19690720)) 5))
     ))
 
 (deftest day3_1_coord_builder
@@ -49,13 +45,11 @@
 (deftest day3_1_manhattan_test
   (testing "Day 3.1 Manhattan distance"
     (is (= (day3_1_manhattan_distance 
-            (day3_1_all_intersections_for_both_lines 
-             (into [] (map #(name %) '[R75,D30,R83,U83,L12,D49,R71,U7,L72])) 
-             (into [] (map #(name %) '[U62,R66,U55,R34,D71,R55,D58,R83])))) 
+            (into [] (map #(name %) '[R75,D30,R83,U83,L12,D49,R71,U7,L72])) 
+            (into [] (map #(name %) '[U62,R66,U55,R34,D71,R55,D58,R83])))
            159))
     (is (= (day3_1_manhattan_distance 
-            (day3_1_all_intersections_for_both_lines 
-             (into [] (map #(name %) '[R8,U5,L5,D3])) 
-             (into [] (map #(name %) '[U7,R6,D4,L4])))) 
+            (into [] (map #(name %) '[R8,U5,L5,D3])) 
+            (into [] (map #(name %) '[U7,R6,D4,L4])))
            6))
     ))
